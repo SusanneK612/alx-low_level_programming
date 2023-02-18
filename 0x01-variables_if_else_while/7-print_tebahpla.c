b6af7d084367e37e13ad3876a5670c9d1a2a16e2
Mews <myemail@gmail.com>
2023-02-17 20:14:25 -0800

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet from z to a
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
