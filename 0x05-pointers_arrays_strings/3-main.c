150af5f98c0ac2fe6a278d3fc58ae2767f4d6913
Mews <myemail@gmail.com>
2023-02-27 10:15:40 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "MAIN.H"
+
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+str = "I do not fear computers.I fear the lack the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
